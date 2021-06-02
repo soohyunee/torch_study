b80a0a0ea4ec27c5766a16bd94129816fa7db82e
Eugene <72247823+soohyunee@users.noreply.github.com>
2021-06-02 16:25:32 +0900

    Delete 02_sequence_to_sequence directory

--- 02_sequence_to_sequence/seq2seq_with_neural_networks/utils.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-def init_weights(m):
-    for name, param in m.named_parameters():
-        nn.init.uniform_(param.data, -0.08, 0.08)
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
